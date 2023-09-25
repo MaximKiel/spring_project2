ac211501a0e1100a690b478377a922cf0512ff49
MaximKiel <loup.du.nord@yandex.ru>
2023-09-25 15:08:45 +0300

    Add populateDB.sql

---
+++ src/main/resources/populateDB.sql
@@ -0,0 +1,11 @@
+INSERT INTO person (name, birth_year) VALUES ('Иван Иванов', 1997);
+INSERT INTO person (name, birth_year) VALUES ('Петр Петров', 2000);
+INSERT INTO person (name, birth_year) VALUES ('Сергей Сергеев', 1993);
+INSERT INTO person (name, birth_year) VALUES ('Семен Семенов', 1985);
+INSERT INTO person (name, birth_year) VALUES ('Алексей Алексеев', 1995);
+
+INSERT INTO book (title, author_name, publish_year) VALUES ('О животных', 'Автор о животных', 2005);
+INSERT INTO book (title, author_name, publish_year) VALUES ('О путешествиях', 'Автор о путешествиях', 2019);
+INSERT INTO book (title, author_name, publish_year) VALUES ('О кино', 'Автор о кино', 2017);
+INSERT INTO book (title, author_name, publish_year) VALUES ('О живописи', 'Автор о живописи', 2010);
+INSERT INTO book (title, author_name, publish_year) VALUES ('О литературе', 'Автор о литературе', 2021);
